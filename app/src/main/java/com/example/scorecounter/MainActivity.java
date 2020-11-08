01eab6966692551c62907c7cc008c473c09499b5
Kiss Dániel <oldtrue69@gmail.com>
2020-11-08 17:44:50 +0100

    MainActivity2 create

--- app/src/main/java/com/example/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/scorecounter/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.scorecounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    public void init() {
+        btnStart.findViewById(R.id.btnStart);
     }
 }
